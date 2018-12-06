e8a581f9eff65a295e5af7f9327afb2c8674f061
Vitamin <vitamin@ukr.net>
2018-12-06 10:57:20 +0200

    CryptoCurrencyProject

---
+++ build/precache-manifest.0e078ed0a5856b59614625b0814b6a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49dd09d7785e9e2fec69",
+    "url": "/CryptocurrencyProject/static/js/runtime~main.49dd09d7.js"
+  },
+  {
+    "revision": "d9242000bb5a9e1cbbc8",
+    "url": "/CryptocurrencyProject/static/js/main.d9242000.chunk.js"
+  },
+  {
+    "revision": "8f33823224f821310b6c",
+    "url": "/CryptocurrencyProject/static/js/1.8f338232.chunk.js"
+  },
+  {
+    "revision": "d9242000bb5a9e1cbbc8",
+    "url": "/CryptocurrencyProject/static/css/main.14378896.chunk.css"
+  },
+  {
+    "revision": "8f33823224f821310b6c",
+    "url": "/CryptocurrencyProject/static/css/1.f2566ae6.chunk.css"
+  },
+  {
+    "revision": "0b94d4cd64d68e3ad04260c7e14d433a",
+    "url": "/CryptocurrencyProject/index.html"
+  }
+];
