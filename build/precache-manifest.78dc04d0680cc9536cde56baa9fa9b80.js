ce06899ef5be80e3d856aa6b80567d533c75cc81
Vitamin <vitamin@ukr.net>
2018-12-06 17:27:35 +0200

    CryptoCurrencyProject

---
+++ build/precache-manifest.78dc04d0680cc9536cde56baa9fa9b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49dd09d7785e9e2fec69",
+    "url": "/CryptocurrencyProject/static/js/runtime~main.49dd09d7.js"
+  },
+  {
+    "revision": "b1dcc0490efedd6c9b40",
+    "url": "/CryptocurrencyProject/static/js/main.b1dcc049.chunk.js"
+  },
+  {
+    "revision": "bc7d8a419dc3c869ddc3",
+    "url": "/CryptocurrencyProject/static/js/1.bc7d8a41.chunk.js"
+  },
+  {
+    "revision": "b1dcc0490efedd6c9b40",
+    "url": "/CryptocurrencyProject/static/css/main.7ec7ac77.chunk.css"
+  },
+  {
+    "revision": "bc7d8a419dc3c869ddc3",
+    "url": "/CryptocurrencyProject/static/css/1.9a593729.chunk.css"
+  },
+  {
+    "revision": "00eaebdacfa3748dd528f83f4b7d54c9",
+    "url": "/CryptocurrencyProject/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/CryptocurrencyProject/precache-manifest.1f9164d698eed30037a6c4298462babe.js"
+  "/CryptocurrencyProject/precache-manifest.78dc04d0680cc9536cde56baa9fa9b80.js"
 );
 
 workbox.clientsClaim();
