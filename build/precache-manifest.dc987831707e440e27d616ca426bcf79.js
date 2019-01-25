8dac04bca9b675ce9831c3a394526447d48510e6
Vitalii Vitaminenko <vitamin@ukr.net>
2019-01-26 00:27:11 +0200

    build

---
+++ build/precache-manifest.dc987831707e440e27d616ca426bcf79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49dd09d7785e9e2fec69",
+    "url": "/CryptocurrencyProject/static/js/runtime~main.49dd09d7.js"
+  },
+  {
+    "revision": "24e23e7482f714466d93",
+    "url": "/CryptocurrencyProject/static/js/main.24e23e74.chunk.js"
+  },
+  {
+    "revision": "69a68fd7b216cf2d4414",
+    "url": "/CryptocurrencyProject/static/js/1.69a68fd7.chunk.js"
+  },
+  {
+    "revision": "24e23e7482f714466d93",
+    "url": "/CryptocurrencyProject/static/css/main.e2827bd9.chunk.css"
+  },
+  {
+    "revision": "69a68fd7b216cf2d4414",
+    "url": "/CryptocurrencyProject/static/css/1.b79bbeb9.chunk.css"
+  },
+  {
+    "revision": "251db418ce9382d8537e6deb56ef0842",
+    "url": "/CryptocurrencyProject/index.html"
+  }
+];
